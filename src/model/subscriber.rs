911b5185ffc986fe9d21bfb8877388e82cdbc98b
raspatimahatma <tommymahatma08@gmail.com>
2024-04-12 16:53:13 +0700

    Create Subscriber model struct.

---
+++ src/model/subscriber.rs
@@ -0,0 +1,13 @@
+use rocket::serde::{Deserialize, Serialize};
+use rocker::serde::json::to_string;
+use rocket::log;
+use rocket::tokio;
+use bambangshop::REQWEST_CLIENT;
+use crate::model:notification::Notification;
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Subscriber {
+    pub url : String,
+    pub name: String,
+}
