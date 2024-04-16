bd4ef0094e4cdfb285f7afcbd3d4c281fcfb508f
raspatimahatma <tommymahatma08@gmail.com>
2024-04-16 21:54:43 +0700

    fix some typo

--- src/service/product.rs
+++ src/service/product.rs
@@ -44,16 +44,6 @@ impl ProductService {
 
         NotificationService.notify(&product.product_type, "DELETED", product.clone());
         return Ok(Json::from(product));
-
-            pub fn publish(id: usize)->Result<Product>{
-        let product_opt: Option<Product> = ProductRepository::get_by_id(id);
-        if product_opt.is_none(){
-            return Err(compose_error_response(Status::NotFound, String::from("Product not found")));
-        }
-        let product: Product = product_opt.unwrap();
-
-        NotificationService.notify(&product.product_type, "PROMOTION", product.clone());
-        return Ok(product);
     }
 
     pub fn publish(id: usize)->Result<Product>{
@@ -67,5 +57,5 @@ impl ProductService {
         return Ok(product);
     }
 
-    }
 }
+
